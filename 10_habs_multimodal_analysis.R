32062f42f47f6b562f863ee03457fbab8afc9fae
vshi2316 <152848467+vshi2316@users.noreply.github.com>
2026-08-14 16:19:39 +0800

    Create 10_habs_multimodal_analysis.R

---
+++ 10_habs_multimodal_analysis.R
@@ -0,0 +1,22 @@
+# HABS-HD multimodal analysis entry point
+
+script_path <- tryCatch(
+  normalizePath(sys.frame(1)$ofile, winslash = "/", mustWork = TRUE),
+  error = function(e) NA_character_
+)
+if (is.na(script_path)) {
+  stop("Run with source('10_habs_multimodal_analysis.R').", call. = FALSE)
+}
+
+root <- dirname(script_path)
+modules <- c(
+  "10_1_habs_multimodal_data.R",
+  "10_2_habs_longitudinal_cognition.R",
+  "10_3_habs_cognition_models.R",
+  "10_4_habs_primary_models.R",
+  "10_5_habs_regional_analysis.R"
+)
+
+for (module in modules) {
+  source(file.path(root, module), encoding = "UTF-8", chdir = TRUE)
+}
